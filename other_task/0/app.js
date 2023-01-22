36b8c50568f432f4c6f44682c95357bf2b24dd53
Wihctoh <rivertoxi911@gmail.com>
2023-01-22 22:52:02 +0300

    some fix

--- other_task/0/app.js
+++ other_task/0/app.js
@@ -1,7 +1,7 @@
 // На вход подаются 2 строки.
 //Необходимо найти в 1 строке 2 подстроку и вывести true, если совпадение есть, в противном случае false
 
-const str1 = prompt("введите строку").toLowerCase().trim();
+const str1 = prompt("введите первую  строку").toLowerCase().trim();
 const str2 = prompt("введите вторую строку").toLowerCase().trim();
 
 if (str1.includes(str2)) {
