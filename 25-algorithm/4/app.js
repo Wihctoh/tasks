355ed8d50ac864c32e0e34730b18dbe76a19510f
Wihctoh <rivertoxi911@gmail.com>
2023-02-21 15:24:54 +0300

    new task

--- 25-algorithm/4/app.js
+++ 25-algorithm/4/app.js
@@ -6,4 +6,16 @@
 // 10 -> 55
 // 7 -> 13
 
-const  num = 10;
+const num = 10;
+
+function fibonachi(num_) {
+  if (num_ == 1) return 1;
+  if (num_ == 0) return 0;
+  if (num > 1) {
+    return fibonachi(num_ - 2) + fibonachi(num_ - 1);
+  }
+}
+
+let result = fibonachi(num);
+
+console.log(result);
