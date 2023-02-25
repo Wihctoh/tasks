ae21607f4e57fab03636cf7de0d9d4ece48f8c59
Wihctoh <rivertoxi911@gmail.com>
2023-02-25 12:12:17 +0300

    fix

--- 26-try_catche/8(repair)/app.js
+++ 26-try_catche/8(repair)/app.js
@@ -1,26 +1,32 @@
 // Реализуйте функцию, которая принимает массив последовательных
 // (возрастающих) букв и возвращает отсутствующую в массиве. Добавить проверки
 // ["a","b","c","d","f"] -> "e"
-// ["O","Q","R","S"] -> "P" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! требует доработки
+// ["O","Q","R","S"] -> "P"
 
 const arrLetter1 = ["a", "b", "c", "d", "f"];
-const arrLetter2 = ["O", "Q", "R", "S"];
+const arrLetter2 = ["O", "Q", "I", "S"].join("").toLowerCase().split("");
 
 function doWords(arrLetter_) {
   try {
     let alphabet = "abcdefghijklmnopqistuvwxyz";
-    let sliceArr = [];
+    let firstLetter = alphabet.indexOf(arrLetter_[0]);
+    let lastLetter = alphabet.lastIndexOf(arrLetter_[arrLetter_.length-1]);
+    let sliceArr = "";
+    let resArr = [];
 
-    for (let i = 0; i < alphabet.length; i++) {
-      if (arrLetter_.includes(alphabet[i])) {
-        sliceArr.push(alphabet[i]);
-      }
-    }
-    return sliceArr;
+    sliceArr += alphabet.slice(firstLetter, lastLetter);
+
+    console.log(arrLetter_);
+    console.log(sliceArr);
+
+    // for (let i = 0; i < sliceArr.length ; i++) {
+      
+    // }
+    return resArr;
   } catch (error) {
     return error.message;
   }
 }
-let result = doWords(arrLetter1);
+let result = doWords(arrLetter2);
 
 console.log(result);
