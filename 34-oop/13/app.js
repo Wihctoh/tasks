e32b3ad9a6717696425e87cae90e6b61afeac2d3
Wihctoh <rivertoxi911@gmail.com>
2023-03-17 22:21:27 +0300

    add 13 task

---
+++ 34-oop/13/app.js
@@ -0,0 +1,87 @@
+// . Реализуйте класс ServerPost. Обязательными функциями считаются функции
+// middleware, controller, service, repository. Цепочка взаимодействия между методами
+// следующая:
+// middleware -> controller -> service -> repository, где:
+// middleware – функция валидатор
+// controller – функция, принимающая данные. Принимает json
+// service – функция проверки на то что с repository вернулось значение
+// repository – функция, симулирующая БД. Хранит массив данных. Взаимодействие с
+// этим массивом осуществляется только в repository. Массив находится в приложении
+// Задание:
+// на вход подается JSON вида:
+// `{
+// "label": "JavaScript", "category": "programmingLanguages", "priority": 1
+// }`
+// Неоходимо добавить в массив БД объект только в том случае, если нет совпадений
+// по label. Если совпадения нет, то в объект клиента добавить ключ id со значением
+// label в toLowerCase таким образом, чтобы в БД был запушен объект вида
+// {"id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1}
+// Если совпадение есть – ошибка. Добавить проверки
+
+class ServerPost {
+  middleware(obj) {
+    let middl = this.controller(obj);
+
+    return middl;
+  }
+  controller(obj) {
+    try {
+      let contr = this.service(obj);
+
+      return contr;
+    } catch (error) {
+      return error.message;
+    }
+  }
+  service(obj) {
+    let serv = this.repository(obj);
+
+    return serv;
+  }
+  repository(obj) {
+    const arr = [
+      {
+        id: "javascript",
+        label: "JavaScript",
+        category: "programmingLanguages",
+        priority: 1,
+      },
+      {
+        id: "typescript",
+        label: "TypeScript",
+        category: "programmingLanguages",
+        priority: 1,
+      },
+      {
+        id: "sql",
+        label: "SQL",
+        category: "programmingLanguages",
+        priority: 2,
+      },
+      {
+        id: "java",
+        label: "Java",
+        category: "programmingLanguages",
+        priority: 3,
+      },
+      { id: "go", label: "GO", category: "programmingLanguages", priority: 3 },
+    ];
+
+    const filterArr = arr.filter(function (el) {
+      el.label == obj.label ? true : null;
+    });
+    return arr;
+  }
+}
+
+const obj = {
+  label: "JavaScript",
+  category: "programmingLanguages",
+  priority: 1,
+};
+
+const serverPost = new ServerPost();
+
+let result = serverPost.controller(obj);
+
+console.log(result);
