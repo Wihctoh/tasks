3d031bfb0c315e439b734feda0e50e1306a5d4ec
wihctoh <rivertoxi911@gmail.com>
2023-03-23 21:52:12 +0300

    fix

--- 35-solid/9/app.js
+++ 35-solid/9/app.js
@@ -1,37 +1,51 @@
 // Реализуйте класс Сonversion, сдержащий метод do DecimalNumber, преобразующий
 // все числа 10 системы счисления в 2
 
-class Conversation {
-  doDecimalNumber(num) {
-    try {
-      let arr = [num];
-      let resultArr = [];
+// class Conversation {
+//   doDecimalNumber(num) {
+//     try {
+//       let arr = [num];
+//       let resultArr = [];
 
-      arr.push(Math.floor(num / 2));
+//       arr.push(Math.floor(num / 2));
 
-      for (let i = 1; i < num; i++) {
-        let floorNum = Math.floor(arr[i] / 2);
+//       for (let i = 1; i < num; i++) {
+//         let floorNum = Math.floor(arr[i] / 2);
 
-        if (floorNum !== 0) {
-          arr.push(floorNum);
-        } else {
-          arr.push(Math.round(arr[i] / 2));
+//         if (floorNum !== 0) {
+//           arr.push(floorNum);
+//         } else {
+//           arr.push(Math.round(arr[i] / 2));
 
-          break;
-        }
-      }
+//           break;
+//         }
+//       }
 
-      for (let i = 0; i < arr.length - 1; i++) {
-        resultArr.push(arr[i] % 2);
-      }
+//       for (let i = 0; i < arr.length - 1; i++) {
+//         resultArr.push(arr[i] % 2);
+//       }
 
-      return resultArr.reverse().join("");
-    } catch (error) {
-      return error.message;
-    }
-  }
+//       return resultArr.reverse().join("");
+//     } catch (error) {
+//       return error.message;
+//     }
+//   }
+// }
+
+// const conversation = new Conversation();
+
+// console.log(conversation.doDecimalNumber(375));
+
+let num = 102;
+let arr = [];
+
+while (num >= 2) {
+  let ot = num % 2;
+  arr.push(ot);
+
+  num = (num - ot) / 2;
 }
 
-const conversation = new Conversation();
+arr.push(num);
 
-console.log(conversation.doDecimalNumber(375));
+console.log(arr.reverse().join(""));
