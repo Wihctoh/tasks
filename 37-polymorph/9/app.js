ba8db77a8466670c1eed4b246353a2402e277018
Wihctoh <rivertoxi911@gmail.com>
2023-04-03 15:51:00 +0300

    add 9 task

--- 37-polymorph/9/app.js
+++ 37-polymorph/9/app.js
@@ -2,7 +2,16 @@
 // распознать по какому из значений контейнера производится событие клика. Если
 // клик происходит, то удалить данное значение из массива в div
 
-document.querySelector("p").addEventListener("click", function (event) {
+let div = document.querySelector("div");
+
+document.querySelector("button").addEventListener("click", () => {
   const p = document.querySelectorAll("p");
 
+  for (let i = 0; i < p.length; i++) {
+    p[i].innerHTML = Math.round(Math.random() * 100);
+  }
+});
+
+div.addEventListener("click", function (event) {
+  event.target.remove();
 });
