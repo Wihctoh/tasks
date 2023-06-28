abfdc69fdb61d8123c6b5b5421ed8d9a4ed40ebb
Wihctoh <rivertoxi911@gmail.com>
2023-06-28 21:24:48 +0300

    add task

--- 62-TS/src/app13-23.ts
+++ 62-TS/src/app13-23.ts
@@ -13,12 +13,40 @@
 //14. Напишите программу, которая находит и выводит наиболее часто встречающийся
 // символ в заданной строке.
 
-const string999: string = "qqqqwwweeer";
-const obj11 = {};
+// const string999: string = "qqqqwwweeer";
+// const obj11 = {};
 
-for (let i = 0; i < string999.length; i++) {
-  if (obj11[string999[i]]) {
-    obj11[string999[i]] += 1;
-  } else obj11[string999[i]] = 1;
+// for (let i = 0; i < string999.length; i++) {
+//   if (obj11[string999[i]]) {
+//     obj11[string999[i]] += 1;
+//   } else obj11[string999[i]] = 1;
+// }
+// const result1111 = Object.entries(obj11);
+
+//15. На входе динамичный массив строк. Используя forEach создайте новый массив из
+// элементов, каждое значение которого имеет вид !name
+// [“hschool”, “company”] -> [“!hschool”, “!company”]
+
+// const str33: string[] = ["hschool", "company"];
+// let resArr: string[] = [];
+
+// str33.forEach((el) => resArr.push("!" + el));
+
+// console.log(resArr);
+
+// 16. Создайте массив чисел и определите, является ли он строго возрастающей
+// последовательностью.
+
+const numArray: number[] = [3, 4, 9, 10, 60, 70];
+const count: number[] = [numArray[0]];
+
+for (let i = 1; i < numArray.length; i++) {
+  if (numArray[i] > count[count.length - 1]) {
+    count.push(numArray[i]);
+  } else {
+    console.log(false);
+    break;
+  }
 }
-const result1111 = Object.entries(obj11);
+
+if (numArray.length === count.length) console.log(true);
