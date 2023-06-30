930c3df3d4309a935329bf80327a8f7aa10b2528
Wihctoh <rivertoxi911@gmail.com>
2023-06-30 09:51:43 +0300

    add new task

--- 62-TS/dist/app13-23.js
+++ 62-TS/dist/app13-23.js
@@ -47,7 +47,11 @@
 // console.log(first - second);
 // 18. Создайте массив объектов, представляющих книги, и найдите книгу где автор
 // Булгаков (author – одно из полей объекта)
-// const arrObj = [
+// interface iArrObj {
+//   author: string;
+//   title: string;
+// }
+// const arrObj: iArrObj[] = [
 //   { author: "Булгаков", title: "Собачье сердце" },
 //   { author: "Гоголь", title: "Мертвые души" },
 //   { author: "Булгаков", title: "Мастер и маргарита" },
@@ -60,27 +64,48 @@
 // }
 // 19. Создайте массив объектов, представляющих книги, и отобразите книги где цена
 // каждой > 50 (price – одно из полей объекта)
-// const arrObj = [
+// interface iArrObj {
+//   author: string;
+//   title: string;
+//   price: number;
+// }
+// const arrObj: iArrObj[] = [
 //   { author: "Булгаков", title: "Собачье сердце", price: 10 },
 //   { author: "Гоголь", title: "Мертвые души", price: 50 },
 //   { author: "Булгаков", title: "Мастер и маргарита", price: 100 },
 //   { author: "Пушкин", title: "Дубровский", price: 60 },
 // ];
-// const filterBook = arrObj.filter((el) => el.price > 50);
+// const filterBook: iArrObj[] = arrObj.filter((el) => el.price > 50);
 // console.log(filterBook);
 // 20.Создайте массив объектов, представляющих книги, и найдите книгу с самым
 // большим количеством страниц (count – одно из полей объекта)
-const arrObj = [
-    { author: "Булгаков", title: "Собачье сердце", price: 10, count: 100 },
-    { author: "Гоголь", title: "Мертвые души", price: 50, count: 200 },
-    { author: "Булгаков", title: "Мастер и маргарита", price: 100, count: 300 },
-    { author: "Пушкин", title: "Дубровский", price: 60, count: 350 },
-];
-let elem = 0;
-for (let i = 0; i < arrObj.length; i++) {
-    if (arrObj[i].count > elem) {
-        elem = arrObj[i].count;
-    }
+// interface iArrObj {
+//   author: string;
+//   title: string;
+//   price: number;
+//   count: number;
+// }
+// const arrObj: iArrObj[] = [
+//   { author: "Булгаков", title: "Собачье сердце", price: 10, count: 100 },
+//   { author: "Гоголь", title: "Мертвые души", price: 50, count: 200 },
+//   { author: "Булгаков", title: "Мастер и маргарита", price: 100, count: 300 },
+//   { author: "Пушкин", title: "Дубровский", price: 60, count: 350 },
+// ];
+// let elem: number = 0;
+// for (let i = 0; i < arrObj.length; i++) {
+//   if (arrObj[i].count > elem) {
+//     elem = arrObj[i].count;
+//   }
+// }
+// const filterBook: iArrObj[] = arrObj.filter((el) => el.count === elem);
+// console.log(filterBook);
+// 21. Напишите программу, которая находит и выводит длину наиболее длинного
+// слова в заданной строке.
+const stringItem = "qwe asd zxczzxczx".split(" ");
+let count = 0;
+for (let i = 0; i < stringItem.length; i++) {
+    if (stringItem[i].length > count)
+        count = stringItem[i].length;
 }
-const filterBook = arrObj.filter((el) => el.count === elem);
-console.log(filterBook);
+const filtered1 = stringItem.filter((el) => el.length === count);
+console.log(`max length word is ${count} in ${filtered1}`);
